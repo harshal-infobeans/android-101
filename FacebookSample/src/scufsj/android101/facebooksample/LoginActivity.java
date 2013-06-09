7da9255600e7498131d01aadea8981f5551b23c8
Bruno Torres <bruno.torres@me.com>
2013-06-09 19:05:12 -0300

    Removendo alguns imports não utilizados.

--- FacebookSample/src/scufsj/android101/facebooksample/LoginActivity.java
+++ FacebookSample/src/scufsj/android101/facebooksample/LoginActivity.java
@@ -1,17 +1,9 @@
 package scufsj.android101.facebooksample;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 
